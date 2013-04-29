90c19fcbe43d037d331bc4c96fd6ccf1bd6b66cc
Missaoui Chedy <missaoui.chedy@gmail.com>
2013-04-29 13:54:37 +0100

    Expermintation proceeding

--- src/Tuto/AnimeauxBundle/Controller/DefaultController.php
+++ src/Tuto/AnimeauxBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
         //throw $this->createNotFoundException('2012 GONNA CHANGE MY CAREER!!!');
         $session = $this->getRequest()->getSession();
         $session->set('value', '2149');
-        return $this->render('TutoAnimeauxBundle:Default:index.html.twig', array('addCarpage' => $this->generateUrl('TutoAnimeauxBundle_addCarpage')));
+        return $this->render('TutoAnimeauxBundle:Default:index.html.twig');
        // return $this->forward('TutoAnimeauxBundle:Add:addCar');
     }
 }
